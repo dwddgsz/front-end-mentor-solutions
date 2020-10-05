131ec2b18c2d20af6da5d609b2bc8311fd35a1cd
dwdlearngit <dwdlearngit@gmail.com>
2020-10-05 17:59:48 +0200

    CHANGE button to div, ADD focus state

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -6,8 +6,8 @@ const mediaQuery = window.matchMedia('(min-width:767px)');
 let changeToShare = () => {};
 // FUNCTION THAT CHANGES CONTENT OF SHARE-SECTION TO AUTHOR-SECTION (CAN BE USED ONLY ON MOBILE DEVICES)
 const changeToAuthor = function () {
-    toggleSection.style.backgroundColor = 'white';
-    toggleSection.innerHTML = `
+  toggleSection.style.backgroundColor = 'white';
+  toggleSection.innerHTML = `
          <div class="author-data">
            <img src="./images/avatar-michelle.jpg" class="author-photo" alt="photo-of-author">
            <div class="author-text">
@@ -15,7 +15,7 @@ const changeToAuthor = function () {
              <p class="author-date"> 28 Jun 2020</p>
            </div>
          </div>
-         <button class="share-button" id="button" onclick="changeToShare()"> <img src="./images/icon-share.svg" alt="share-button">
+         <div class="share-button" id="button" tabindex="0" onclick="changeToShare()"> <img src="./images/icon-share.svg" alt="share-button">
            <div class="share-popup" id="share-popup">
              <div class="share-popup-triangle"></div>
              <div class="share-data">
@@ -26,16 +26,16 @@ const changeToAuthor = function () {
                  <img src="./images/icon-pinterest.svg" alt="pinterest-icon">
                </div>
              </div>
-           </div> </button>
+           </div> </div>
          `
 }
 // FUNCTION THAT CHANGES HOW changeToShare WORKS (FOR MOBILE)
 const changeForMobile = () => {
-    const popup = document.getElementById('share-popup');
-    popup.classList.remove('popup-visible');
-    changeToShare = () => {
-        toggleSection.style.backgroundColor = 'hsl(217, 19%, 35%)';
-        toggleSection.innerHTML = `<div class="share-data">
+  const popup = document.getElementById('share-popup');
+  popup.classList.remove('popup-visible');
+  changeToShare = () => {
+    toggleSection.style.backgroundColor = 'hsl(217, 19%, 35%)';
+    toggleSection.innerHTML = `<div class="share-data">
          <p class="share-text">Share</p>
          <div class="share-icons-container">
            <img src="./images/icon-facebook.svg" alt="facebook-icon">
@@ -43,25 +43,25 @@ const changeForMobile = () => {
            <img src="./images/icon-pinterest.svg" alt="pinterest-icon">
          </div>
        </div>
-       <button class="share-button" id="button" onclick="changeToAuthor()"> <img src="./images/icon-share.svg" alt="share-button">
-       </button>`;
-    }
+       <div class="share-button" id="button" tabindex="0" onclick="changeToAuthor()"> <img src="./images/icon-share.svg" alt="share-button">
+       </div>`;
+  }
 }
 // FUNCTION THAT CHANGES HOW changeToShare WORKS (FOR DESKTOP). IT ALSO INVOKES changeToAuthor SO USER SECTION IS ALWAYS VISIBLE ON DESKTOP
 const changeForDesktop = () => {
-    changeToAuthor();
-    changeToShare = () => {
-        const popup = document.getElementById('share-popup');
-        popup.classList.toggle('popup-visible')
-    }
+  changeToAuthor();
+  changeToShare = () => {
+    const popup = document.getElementById('share-popup');
+    popup.classList.toggle('popup-visible')
+  }
 }
 // FUNCTION THAT CHECKS FOR CHANGES DEPENDING ON MEDIA QUERIES
 const checkForChanges = () => {
-    if (mediaQuery.matches) {
-        changeForDesktop();
-    } else {
-        changeForMobile();
-    }
+  if (mediaQuery.matches) {
+    changeForDesktop();
+  } else {
+    changeForMobile();
+  }
 }
 // INITIAL CHECK FOR MEDIA QUERIES
 checkForChanges();
